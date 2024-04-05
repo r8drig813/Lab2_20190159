7c9fc67371732d6e3260e6cfd3dd186948073c40
Rodrigo Tinoco <a20190159@pucp.edu.pe>
2024-04-04 20:00:23 -0500

    first commit

---
+++ app/src/main/java/com/example/lab2_appiot/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.lab2_appiot;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final int[] colores = {R.color.color1, R.color.color2, R.color.color3}; // Agrega tantos colores como desees
+
+        final int[] colorIndex = {0};
+
+        TextView textView = findViewById(R.id.textView3);
+
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Cambiar el color del texto al siguiente color en el array
+                textView.setTextColor(getResources().getColor(colores[colorIndex[0]]));
+
+                // Incrementar el índice del color
+                colorIndex[0]++;
+
+                // Si llegamos al final del array, volvemos al principio
+                if (colorIndex[0] >= colores.length) {
+                    colorIndex[0] = 0;
+                }
+            }
+        });
+
+
+    }
+
+
+
+
+    public void irIndicaciones(View view) {
+        Intent intent = new Intent(this, MainActivity2.class);
+        startActivity(intent);
+    }
+
+    public void irCalcular(View view) {
+        Intent intent = new Intent(this, MainActivity3.class);
+        startActivity(intent);
+    }
+
+
+}
---
+++ app/src/main/java/com/example/lab2_appiot/MainActivity3.java
@@ -0,0 +1,112 @@
+package com.example.lab2_appiot;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import com.google.android.material.button.MaterialButton;
+
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import org.mozilla.javascript.Context;
+import org.mozilla.javascript.Scriptable;
+
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity3 extends AppCompatActivity implements View.OnClickListener{
+
+    TextView resultTv,solutionTv;
+    MaterialButton buttonC;
+    MaterialButton buttonDivide,buttonMultiply,buttonPlus,buttonMinus,buttonEquals;
+    MaterialButton button0,button1,button2,button3,button4,button5,button6,button7,button8,button9;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+        resultTv = findViewById(R.id.result_tv);
+        solutionTv = findViewById(R.id.solution_tv);
+
+        assignId(buttonC,R.id.buttonClear);
+        assignId(buttonDivide,R.id.buttonDivide);
+        assignId(buttonMultiply,R.id.buttonMultiply);
+        assignId(buttonPlus,R.id.buttonAdd);
+        assignId(buttonMinus,R.id.buttonSubtract);
+        assignId(buttonEquals,R.id.buttonEquals);
+        assignId(button0,R.id.button0);
+        assignId(button1,R.id.button1);
+        assignId(button2,R.id.button2);
+        assignId(button3,R.id.button3);
+        assignId(button4,R.id.button4);
+        assignId(button5,R.id.button5);
+        assignId(button6,R.id.button6);
+        assignId(button7,R.id.button7);
+        assignId(button8,R.id.button8);
+        assignId(button9,R.id.button9);
+
+    }
+
+    void assignId(MaterialButton btn,int id){
+        btn = findViewById(id);
+        btn.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        MaterialButton button =(MaterialButton) view;
+        String buttonText = button.getText().toString();
+        String dataToCalculate = solutionTv.getText().toString();
+
+
+        if(buttonText.equals("=")){
+            solutionTv.setText(resultTv.getText());
+            return;
+        }
+        if(buttonText.equals("C")){
+            dataToCalculate = dataToCalculate.substring(0,dataToCalculate.length()-1);
+        }else{
+            dataToCalculate = dataToCalculate+buttonText;
+        }
+        solutionTv.setText(dataToCalculate);
+
+        String finalResult = getResult(dataToCalculate);
+
+        if(!finalResult.equals("Err")){
+            resultTv.setText(finalResult);
+        }
+
+    }
+
+    String getResult(String data){
+        try{
+            Context context  = Context.enter();
+            context.setOptimizationLevel(-1);
+            Scriptable scriptable = context.initStandardObjects();
+            String finalResult =  context.evaluateString(scriptable,data,"Javascript",1,null).toString();
+            if(finalResult.endsWith(".0")){
+                finalResult = finalResult.replace(".0","");
+            }
+            return finalResult;
+        }catch (Exception e){
+            return "Err";
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return true;
+    }
+
+    public void irHistorial(MenuItem item) {
+        Intent intent = new Intent(this, MainActivity4.class);
+        startActivity(intent);
+    }
+}
